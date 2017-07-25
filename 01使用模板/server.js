bb6d02c5d4d2e01a75c6e139c50fcedeec27591c
严俊东 <1172482914@qq.com>
2017-07-25 21:24:45 +0800

    模板插值

--- 01使用模板/server.js
+++ 01使用模板/server.js
@@ -6,6 +6,14 @@ const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer({
   template: require('fs').readFileSync('./index.template.html', 'utf-8')
 })
+
+const context = {
+  title: 'vue-ssr',
+  meta: `
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  `
+}
+
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
@@ -13,7 +21,7 @@ server.get('*', (req, res) => {
     },
     template: `<div>访问的 URL 是： {{ url }}</div>`
   })
-  renderer.renderToString(app, (err, html) => {
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
