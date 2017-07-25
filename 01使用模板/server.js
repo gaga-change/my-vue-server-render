69f3211553eb4ad0fb45c84196c73bb02429c911
严俊东 <1172482914@qq.com>
2017-07-25 21:17:29 +0800

    使用模板

--- 01使用模板/server.js
+++ 01使用模板/server.js
@@ -3,7 +3,9 @@
  */
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
@@ -16,14 +18,7 @@ server.get('*', (req, res) => {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <meta charset='utf-8'>
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(html)
   })
 })
 server.listen(8080)
